3722a3358f1331c96256a4c50905f2651ca67e1e
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-01-09 15:19:52 +0500

    undo commit

--- script.js
+++ script.js
@@ -4,35 +4,27 @@ const au = document.querySelector('#author');
 const book = document.querySelector('#content');
 const data = JSON.parse(localStorage.getItem('bookData')) || [];
 
-class Methods {
-  addItem = (val1, val2) => {
-    if (val1 && val2) {
-      const item = {
-        title: val1,
-        author: val2,
-      };
-      data.push(item);
-      localStorage.setItem('bookData', JSON.stringify(data));
-    }
-  }
-
-  deleteItem = (name, author) => {
-    data.filter((val, ind, arr) => {
-      if (val.title === name && val.author === author) {
-        arr.splice(ind, 1);
-        window.location.reload();
-        return true;
-      }
-      return false;
-    });
-    localStorage.setItem('bookData', JSON.stringify(data));
-  };
-}
-
-const myMethod = new Methods();
 btn.addEventListener('click', () => {
-  myMethod.addItem(ti.value, au.value);
+  const item = {
+    title: ti.value,
+    author: au.value,
+  };
+  data.push(item);
+  localStorage.setItem('bookData', JSON.stringify(data));
 });
+
+// functionality to delete item from the book array and local storage
+const deleteItem = (name, author) => {
+  data.filter((val, ind, arr) => {
+    if (val.title === name && val.author === author) {
+      arr.splice(ind, 1);
+      window.location.reload();
+      return true;
+    }
+    return false;
+  });
+  localStorage.setItem('bookData', JSON.stringify(data));
+};
 
 data.forEach((item) => {
   const h1 = book.appendChild(document.createElement('h1'));
@@ -43,6 +35,6 @@ data.forEach((item) => {
   button.innerText = 'Remove';
   book.appendChild(document.createElement('hr'));
   button.addEventListener('click', () => {
-    myMethod.deleteItem(item.title, item.author);
+    deleteItem(item.title, item.author);
   });
 });
